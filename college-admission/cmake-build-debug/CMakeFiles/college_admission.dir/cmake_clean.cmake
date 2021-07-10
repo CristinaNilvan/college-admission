ace6fbf33add49b18807e6528ef42f85d2d2465b
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-07-10 16:48:02 +0300

    Create college-admission

---
+++ college-admission/cmake-build-debug/CMakeFiles/college_admission.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/college_admission.dir/main.c.obj"
+  "college_admission.exe"
+  "college_admission.exe.manifest"
+  "college_admission.pdb"
+  "libcollege_admission.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/college_admission.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
